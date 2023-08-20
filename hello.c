9ed99e798fd5a4ff9534fb46ad15f48a9330ff45
Berenice Daly <bdaly5@unl.edu>
2023-08-20 16:20:59 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Berenice Daly
+ * Date: 2023/08/20
+ * hack1.0
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Berenice Daly\n");
+
+    return 0;
+}
