45579f980f6df6cbb49fef467d9e1a406befeb70
bdaly5 <142411886+bdaly5@users.noreply.github.com>
2023-08-22 04:57:50 +0000

    Mon, Aug 21, 2023, 11:57 PM -05:00

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
  * Author: Berenice Daly
- * Date: 2023/08/20
+ * Date: 2023-08-20
  *
  * hack1.0
  *
@@ -10,8 +10,9 @@
 
 int main(int argc, char **argv) {
 
-    printf("Hello, my name is Berenice Daly!\n");
-    printf("I am a non-degree grad student.\n");
+    printf("\n\nHello, my name is Berenice Daly!\n");
+    printf("I am currently a non-degree grad student.\n");
+    printf("However, I was a mechanical engineering student for my bachelor's degree and a math education student for my master's degree.\n\n");
 
     return 0;
 }
