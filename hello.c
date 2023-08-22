85181932e96d0abbbbb588f2bb46179e1e7e26eb
Tech Chair <78823621+phi-kap-tech@users.noreply.github.com>
2023-08-22 14:42:50 -0500

    added new name and major

--- hello.c
+++ hello.c
@@ -14,5 +14,9 @@ int main(int argc, char **argv) {
     printf("I am currently a non-degree grad student.\n");
     printf("However, I was a mechanical engineering student for my bachelor's degree and a math education student for my master's degree.\n\n");
 
+    printf("Hello, my name is Benjamin Wormsley!\n");
+    printf("I am a computer engineer in my sophmore year.\n");
+    printf("I am working as an LA for CSCE 155E\n\n");
+
     return 0;
 }
